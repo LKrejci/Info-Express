d381fad6eb049b218647fdcc1b435856a2582ef4
LKrejci <lucaskrejciferreira@gmail.com>
2024-08-25 17:32:35 -0300

    project structure defined

--- app/src/main/java/com/example/infoexpress/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/infoexpress/ui/mainActivity/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.example.infoexpress
+package com.example.infoexpress.ui.mainActivity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,7 +15,6 @@ import com.example.infoexpress.ui.theme.InfoExpressTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             InfoExpressTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
